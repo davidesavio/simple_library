ff9bbb72e6013a26bb50abaef873bf5b06af2c43
Davide Savioli <savioli.davide@gmail.com>
2022-10-14 21:55:39 +0200

    add update/remove book buttons and functions

--- main.js
+++ main.js
@@ -14,9 +14,17 @@ let btnCancel = document.getElementById('btnCancel')
 let form = document.getElementById('form-container')
 let booksContainer = document.querySelector('main')
 let formIsVisible = false
+let bookToUpdateIndex = false
+let title = document.getElementById('title')
+let author = document.getElementById('author')
+let publisher = document.getElementById('publisher')
+let year = document.getElementById('year')
+let pages = document.getElementById('pages')
+let readed = document.getElementById('readed')
 btnNewBook.addEventListener('click', toggleForm)
 btnAdd.addEventListener('click', addBookToLibrary)
-btnCancel.addEventListener('click', closeForm)
+btnCancel.addEventListener('click', toggleForm)
+
 
 
 function Book(title, author, publisher, year, pages, readed) {
@@ -29,29 +37,57 @@ function Book(title, author, publisher, year, pages, readed) {
 }
 
 function addBookToLibrary(e) {
-  e.preventDefault()
-  let formElements = [...e.path[2].elements]
-  let newBook = new Book()
-  formElements.forEach((element) => {
-    if (element.name === 'readed') newBook[element.name] = element.checked
-    else newBook[element.name] = element.value
-  })
-  myLibrary.push(newBook)
-  addBookToDOM()
-  toggleForm()
+    
+    let newBook = new Book(
+        title.value,
+        author.value,
+        publisher.value,
+        year.value,
+        pages.value,
+        readed.checked
+    )
+    if (bookToUpdateIndex) {
+        myLibrary[bookToUpdateIndex] = newBook
+        bookToUpdateIndex = false
+    }
+    else {
+        myLibrary.push(newBook)
+    }
+    addBookToDOM()
+    toggleForm(e)
 }
 
 function addBookToDOM() {
     booksContainer.innerHTML = ''
-    myLibrary.forEach((book) => {
+    createBook()
+    addListenersToButtons()
+}
+
+function addListenersToButtons() {
+    let btnsUpdate = [...document.getElementsByClassName('btnUpdate')]
+    let btnsRemove = [...document.getElementsByClassName('btnRemove')]
+    btnsUpdate.forEach(b => {
+        b.addEventListener('click', updateBook)
+    })
+    btnsRemove.forEach(b => {
+        b.addEventListener('click', removeBook)
+    })
+}
+
+function createBook() {
+    myLibrary.forEach((book, i) => {
         booksContainer.innerHTML += `
-        <div class="book-card">
+        <div id="${i}" class="book-card">
             <p>${book.author}</p>
             <p>${book.title}</p>
             <p>${book.publisher}</p>
             <p>first published in ${book.year}</p>
             <p>${book.pages} pages</p>
             <p>${book.readed?'readed':'not yet finished'}</p>
+            <div class="bookButtons">
+                <button class="btnUpdate">update</button>
+                <button class="btnRemove">remove</button>
+            </div>
         </div>
         `
     })
@@ -59,13 +95,43 @@ function addBookToDOM() {
 
 addBookToDOM()
 
-function toggleForm() {
+function toggleForm(e) {
+    e.preventDefault()
     formIsVisible = !formIsVisible
     if (formIsVisible) form.style.display = 'block'
-    else form.style.display = 'none'
+    else {
+        cleanForm(e)
+        form.style.display = 'none'
+    }
 }
 
-function closeForm(e) {
-    e.preventDefault()
-    toggleForm()
+function cleanForm(e) {
+    // console.log(e);
+    let formElements = [...e.path[2].elements]
+    formElements.forEach(element => {
+        if (element.name === 'readed') element.checked = false
+        else element.value = ''
+    })
+}
+
+function removeBook(e) {
+    let bookToRemove = myLibrary[e.path[2].id]
+    myLibrary = myLibrary.filter(book => book !== bookToRemove)
+    addBookToDOM()
+}
+
+function updateBook(e) {
+    bookToUpdateIndex = e.path[2].id
+    let bookToUpdate = myLibrary[e.path[2].id]
+    toggleForm(e)
+    fillForm(bookToUpdate)
+}
+
+function fillForm(book) {
+    title.value = book.title
+    author.value = book.author
+    publisher.value = book.publisher
+    year.value = book.year
+    pages.value = book.pages
+    readed.checked = book.readed
 }
